4937aac75a92b9888d425ac1f4a15ef09479b303
Toussaint CARLOTTI <toussaint.carlotti@gmail.com>
2024-07-19 15:09:13 +0200

    Ajout de la personnalisation du paramétrage cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5174'],
+    'allowed_origins' => [env('ALLOWED_ORIGINS', '*')],
 
     'allowed_origins_patterns' => [],
 
